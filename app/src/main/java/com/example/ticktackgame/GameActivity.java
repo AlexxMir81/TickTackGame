0326436ebe03deccce3ca8ec52983f6993543f0f
alexx <_alexx@mail.ru>
2024-01-10 22:03:04 +0300

    1 Mode ready

--- app/src/main/java/com/example/ticktackgame/GameActivity.java
+++ app/src/main/java/com/example/ticktackgame/GameActivity.java
@@ -35,19 +35,66 @@ public class GameActivity extends AppCompatActivity {
                    if(button.getText()=="") {
                        if (step % 2 == 0) {
                            button.setText("X");
-                           step++;
                        } else {
                            button.setText("O");
-                           step++;
                        }
                    }
-                    View childView = gridLayout.getChildAt(0 * gridLayout.getColumnCount() + 0);
-                    Button button1 = (Button) childView;
-                    Toast.makeText(GameActivity.this,button1.getText().toString(),Toast.LENGTH_LONG).show();
+                    step++;
+                    if(CheckWinner()){
+                        if (step % 2 == 0){
+                            Toast.makeText(GameActivity.this, "Победитель: O", Toast.LENGTH_LONG).show();
+                        }else {
+                            Toast.makeText(GameActivity.this, "Победитель: X", Toast.LENGTH_LONG).show();
+                        }
+                        ClearGrid();
+                    };
                 }
             });
         }
         
         gridLayout.setBackgroundColor(Color.BLACK);
+    }
+
+    public void ClearGrid(){
+        for (int i = 0; i < gridLayout.getChildCount(); i++){
+            View childView = gridLayout.getChildAt(i);
+            Button button = (Button) childView;
+            button.setText("");
+        }
+
+    }
+    public boolean CheckWinner(){
+        int rowCount = gridLayout.getRowCount();
+        int colCount = gridLayout.getColumnCount();
+        String[][] grid = new String[rowCount][colCount];
+        for (int i = 0; i < rowCount; i++){
+            for (int j = 0; j < colCount; j++){
+                View childView = gridLayout.getChildAt(j * gridLayout.getColumnCount() + i);
+                Button button = (Button) childView;
+                grid[i][j] = button.getText().toString();
+            }
+        }
+
+        for (int i = 0; i < rowCount; i++){
+            if(!grid[i][0].equals("") && grid[i][0].equals(grid[i][1]) && grid[i][0].equals(grid[i][2])){
+                return true;
+            }
+        }
+        for (int i = 0; i < colCount; i++){
+            if(!grid[0][i].equals("") && grid[0][i].equals(grid[1][i]) && grid[0][i].equals(grid[2][i])){
+                return true;
+            }
+        }
+        for (int i = 0; i < rowCount; i++) {
+            if (!grid[0][0].equals("") && grid[0][0].equals(grid[1][1]) && grid[0][0].equals(grid[2][2])) {
+                return true;
+            }
+        }
+        for (int i = 0; i < rowCount; i++) {
+            if (!grid[0][2].equals("") && grid[0][2].equals(grid[1][1]) && grid[0][2].equals(grid[2][0])) {
+                return true;
+            }
+        }
+        return false;
     }
 }
